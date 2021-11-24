607f660d4518b18a841d5666e5c7380135f69abd
local ubuntu <guxiaowa77@gmail.com>
2021-11-24 08:38:35 +0800

    准备做测试

---
+++ plugins/test/hook.py
@@ -0,0 +1,13 @@
+name = 'test'
+description = '测试用'
+address = None
+
+
+async def enable(services):
+    app = services.get('app_svc')
+    # TODO 新增计划任务
+    # TODO 数据库增删改查
+    # TODO notion新增数据查看数据
+    pass
+
+def do():
