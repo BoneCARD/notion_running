25341553b6a5499a4f2cac0f8d6cd0c3cb7dcb4b
b0ne <b0ne@sangfor.cn>
2021-11-26 18:04:51 +0800

    正在写微信公众号文章爬取加处理的逻辑

--- plugins/infospider/app/spider_svc.py
+++ plugins/infospider/app/spider_svc.py
@@ -6,6 +6,11 @@ from bs4 import BeautifulSoup
 from notion_running.plugins.infospider.app.interfaces.i_spider import SpiderInterface
 
 
+# 逛新闻网站的步骤
+# 1、打开网站主页，找有价值的新闻
+# 2、打开自己喜欢的分类，找有价值的新闻
+# 巡视主站
+
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 sem = asyncio.Semaphore(10)  # 信号量，控制协程数，防止爬的过快
 
